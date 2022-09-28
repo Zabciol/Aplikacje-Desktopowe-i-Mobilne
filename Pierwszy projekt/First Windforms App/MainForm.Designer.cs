4f85906b173044517f96c38e1cc89885b3ee3cac
Krzysztof Komisarczyk <19d.krzysztof.komisarczyk@ezse.pl>
2022-09-28 09:04:00 +0200

    Koniec Lekcji

--- Pierwszy projekt/First Windforms App/MainForm.Designer.cs
+++ Pierwszy projekt/First Windforms App/MainForm.Designer.cs
@@ -30,11 +30,12 @@ namespace First_Windforms_App
         private void InitializeComponent()
         {
             this.buttonFirst = new System.Windows.Forms.Button();
+            this.buttonHello = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonFirst
             // 
-            this.buttonFirst.Location = new System.Drawing.Point(517, 32);
+            this.buttonFirst.Location = new System.Drawing.Point(752, 30);
             this.buttonFirst.Name = "buttonFirst";
             this.buttonFirst.Size = new System.Drawing.Size(313, 221);
             this.buttonFirst.TabIndex = 0;
@@ -42,11 +43,21 @@ namespace First_Windforms_App
             this.buttonFirst.UseVisualStyleBackColor = true;
             this.buttonFirst.Click += new System.EventHandler(this.buttonFirst_Click);
             // 
+            // buttonHello
+            // 
+            this.buttonHello.Location = new System.Drawing.Point(182, 165);
+            this.buttonHello.Name = "buttonHello";
+            this.buttonHello.Size = new System.Drawing.Size(269, 154);
+            this.buttonHello.TabIndex = 1;
+            this.buttonHello.Text = "button1";
+            this.buttonHello.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(859, 561);
+            this.ClientSize = new System.Drawing.Size(1119, 561);
+            this.Controls.Add(this.buttonHello);
             this.Controls.Add(this.buttonFirst);
             this.Name = "MainForm";
             this.Text = "Pierwszy Program Okienkowy";
@@ -58,6 +69,7 @@ namespace First_Windforms_App
         #endregion
 
         private System.Windows.Forms.Button buttonFirst;
+        private System.Windows.Forms.Button buttonHello;
     }
 }
 

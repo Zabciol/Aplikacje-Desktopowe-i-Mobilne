7aeb13f315444ce0757107a1e8575fee516d0774
Zabciol <104020296+Zabciol@users.noreply.github.com>
2022-11-23 08:34:29 +0100

    Update

---
+++ Pierwszy projekt/FirstWinFormsApp/MainForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace FirstWinFormsApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonFirst = new System.Windows.Forms.Button();
+            this.buttonHello = new System.Windows.Forms.Button();
+            this.labelName = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.buttonHelloName = new System.Windows.Forms.Button();
+            this.labelAge = new System.Windows.Forms.Label();
+            this.textBoxAge = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonFirst
+            // 
+            this.buttonFirst.Font = new System.Drawing.Font("Segoe UI", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.buttonFirst.Location = new System.Drawing.Point(63, 353);
+            this.buttonFirst.Name = "buttonFirst";
+            this.buttonFirst.Size = new System.Drawing.Size(216, 114);
+            this.buttonFirst.TabIndex = 0;
+            this.buttonFirst.Text = "Przycisk";
+            this.buttonFirst.UseVisualStyleBackColor = true;
+            // 
+            // buttonHello
+            // 
+            this.buttonHello.Font = new System.Drawing.Font("Showcard Gothic", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.buttonHello.Location = new System.Drawing.Point(46, 502);
+            this.buttonHello.Name = "buttonHello";
+            this.buttonHello.Size = new System.Drawing.Size(281, 103);
+            this.buttonHello.TabIndex = 1;
+            this.buttonHello.Text = "Hello";
+            this.buttonHello.UseVisualStyleBackColor = true;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.labelName.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.labelName.Location = new System.Drawing.Point(98, 36);
+            this.labelName.Name = "labelName";
+            this.labelName.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.labelName.Size = new System.Drawing.Size(148, 37);
+            this.labelName.TabIndex = 2;
+            this.labelName.Text = "Podaj imie:";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(98, 76);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(251, 23);
+            this.textBoxName.TabIndex = 3;
+            // 
+            // buttonHelloName
+            // 
+            this.buttonHelloName.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonHelloName.Location = new System.Drawing.Point(98, 164);
+            this.buttonHelloName.Name = "buttonHelloName";
+            this.buttonHelloName.Size = new System.Drawing.Size(251, 68);
+            this.buttonHelloName.TabIndex = 4;
+            this.buttonHelloName.Text = "Przywitaj się";
+            this.buttonHelloName.UseVisualStyleBackColor = true;
+            this.buttonHelloName.Click += new System.EventHandler(this.buttonHelloName_Click);
+            // 
+            // labelAge
+            // 
+            this.labelAge.AutoSize = true;
+            this.labelAge.Location = new System.Drawing.Point(98, 102);
+            this.labelAge.Name = "labelAge";
+            this.labelAge.Size = new System.Drawing.Size(67, 15);
+            this.labelAge.TabIndex = 5;
+            this.labelAge.Text = "Podaj wiek:";
+            // 
+            // textBoxAge
+            // 
+            this.textBoxAge.Location = new System.Drawing.Point(98, 120);
+            this.textBoxAge.Name = "textBoxAge";
+            this.textBoxAge.Size = new System.Drawing.Size(251, 23);
+            this.textBoxAge.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 661);
+            this.Controls.Add(this.textBoxAge);
+            this.Controls.Add(this.labelAge);
+            this.Controls.Add(this.buttonHelloName);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.buttonHello);
+            this.Controls.Add(this.buttonFirst);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Pierwszy program okienkowy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFirst;
+        private System.Windows.Forms.Button buttonHello;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Button buttonHelloName;
+        private System.Windows.Forms.Label labelAge;
+        private System.Windows.Forms.TextBox textBoxAge;
+    }
+}
+
---
+++ Pierwszy projekt/FirstWinFormsApp/MainForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstWinFormsApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonFirst_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Witaj w pierwszym programie WinForms");
+        }
+
+        private void buttonHelloName_Click(object sender, EventArgs e)
+        {
+            string strAge = textBoxAge.Text;
+            if (String.IsNullOrWhiteSpace(strAge))
+            {
+                MessageBox.Show("Nie podano wieku");
+                return;
+            }
+
+            int age;//= int.Parse(strAge);
+            if (!int.TryParse(strAge, out age))
+            {
+                MessageBox.Show("Wiek nie jest liczbą");
+                return;
+            }
+
+            if (age < 1)
+            {
+                MessageBox.Show("Podano wiek ujemny");
+                return;
+            }
+
+            string message = "";
+            if (age >= 18)
+                message = "Jesteś pełnoletni.";
+            else
+                message = "Jesteś niepełnoletni";
+
+           message = "Witaj " + textBoxName.Text + " w tym programie\n" + message;
+            MessageBox.Show(message);
+        }
+    }
+}

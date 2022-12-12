1fbbf25c23c020c1c321629556f3ac7f2047f31c
Zabciol <104020296+Zabciol@users.noreply.github.com>
2022-12-12 14:07:48 +0100

    finished

--- Sprawdzian_3D/Form1.Designer.cs
+++ Sprawdzian_3D/Form1.Designer.cs
@@ -139,7 +139,7 @@ namespace Sprawdzian_3D
             this.Controls.Add(this.labelDane);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Sprawdzian_3D/Form1.cs
+++ Sprawdzian_3D/Form1.cs
@@ -17,6 +17,21 @@ namespace Sprawdzian_3D
             InitializeComponent();
         }
 
+        private void buttonCheck_Click(object sender, EventArgs e)
+        {
+            double a, b, c;
+            bool isRight = false;
+
+            tryParse(out a, out b, out c, ref isRight);
+
+            if (isRight)
+            {
+                getResults(a, b, c);
+            }
+            else
+                error();
+
+        }
 
         private void tryParse(out double a, out double b, out double c ,ref bool isRight)
         {
@@ -32,6 +47,22 @@ namespace Sprawdzian_3D
             }
         }
 
+        private void getResults(double a, double b, double c)
+        {
+            double delta = getDelta(a, b, c);
+            if (delta >= 0)
+            {
+                double deltaPierwiastek = Math.Sqrt(delta);
+                double x1 = getX1(deltaPierwiastek, a, b, c);
+                double x2 = getX2(deltaPierwiastek, a, b, c);
+                labelResults.Text = "x1 = " + x1 + ", x2 = " + x2;
+            }
+            else
+            {
+                error();
+            }
+        }
+
         private double getDelta(double a, double b, double c)
         {
             double delta = (b * b) - (4 * a * c);
@@ -44,74 +75,15 @@ namespace Sprawdzian_3D
             return x1;
         }
 
-
         private double getX2(double deltaPierwiastek, double a, double b, double c)
         {
             double x2 = (-b + deltaPierwiastek) / (2 * a);
             return x2;
         }
 
-
-        private void buttonCheck_Click(object sender, EventArgs e)
+        private void error()
         {
-            double a, b ,c ; 
-            bool isRight = false;
-
-            tryParse(out a, out b, out c, ref isRight);
-            if(isRight)
-            { 
-                double delta = getDelta(a, b, c);
-                if(delta >= 0)
-                {
-                    double deltaPierwiastek = Math.Sqrt(delta);
-                    double x1 = getX1(deltaPierwiastek, a, b, c);
-                    double x2 = getX2(deltaPierwiastek, a, b, c);
-                    labelResults.Text = "x1 = " + x1 + ", x2 = " + x2;
-                }
-                else
-                {
-                    labelResults.Text = "Wprowadź poprawne dane";
-                }
-            }
-            else
-            {
-                labelResults.Text = "Wprowadź poprawne dane";
-            }
-        }
-
-
-        //private void buttonCheck_Click(object sender, EventArgs e)
-        //{
-        //    double a;
-        //    double b;
-        //    double c;
-
-        //    if(    double.TryParse(textBoxDataA.Text, out a) == false   
-        //        || double.TryParse(textBoxDataB.Text, out b) == false
-        //        || double.TryParse(textBoxDataC.Text, out c) == false)
-        //    {
-        //        labelResults.Text = "Wprowadź poprawne dane!";
-        //    }
-        //    else
-        //    {
-        //        double delta = getDelta(a, b, c);
-        //        if (delta >= 0)
-        //        {
-        //            double deltaPierwiastek = Math.Sqrt(delta);
-        //            double x1 = getX1(deltaPierwiastek, a, b, c);
-        //            double x2 = getX2(deltaPierwiastek, a, b, c);
-        //            labelResults.Text = "x1 = " + x1 + ", x2 = " + x2;
-        //        }
-        //        else
-        //        {
-        //            labelResults.Text = "Wprowadź poprawne dane!";
-        //        }
-        //    }
-        //}
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
+            labelResults.Text = "Wprowadź poprawne dane";
+        }    
     }
 }

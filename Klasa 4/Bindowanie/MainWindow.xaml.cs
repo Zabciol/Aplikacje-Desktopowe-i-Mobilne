479de3d23535cdacd4655f5513ecea4fa28763b8
Zabciol <104020296+Zabciol@users.noreply.github.com>
2023-10-10 10:17:09 +0200

    Zadanie

--- Klasa 4/Bindowanie/MainWindow.xaml.cs
+++ Klasa 4/Bindowanie/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,10 @@ namespace Bindowanie
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     /// 
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window , INotifyPropertyChanged
     {
         public string SourceValue { get; set; }
+        public string DayOfTheWeek { get; set; }
 
 
 
@@ -31,9 +33,46 @@ namespace Bindowanie
             InitializeComponent();
         }
 
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show(SourceValue);
+            
+
+            if(SourceValue == "1")
+            {
+                DayOfTheWeek = "Poniedziałek";
+                OnPropertyChanged(nameof(DayOfTheWeek));
+            } if(SourceValue == "2")
+            {
+                DayOfTheWeek = "Wtorek";
+                OnPropertyChanged(nameof(DayOfTheWeek));
+            } if(SourceValue == "3")
+            {
+                DayOfTheWeek = "Środa";
+                OnPropertyChanged(nameof(DayOfTheWeek));
+            } if(SourceValue == "4")
+            {
+                DayOfTheWeek = "Czwartek";
+                OnPropertyChanged(nameof(DayOfTheWeek));
+            } if(SourceValue == "5")
+            {
+                DayOfTheWeek = "Piątek";
+                OnPropertyChanged(nameof(DayOfTheWeek));
+            } if(SourceValue == "6")
+            {
+                DayOfTheWeek = "Sobota";
+                OnPropertyChanged(nameof(DayOfTheWeek));
+            } if(SourceValue == "7")
+            {
+                DayOfTheWeek = "Niedziela";
+                OnPropertyChanged(nameof(DayOfTheWeek));
+            }
         }
     }
 }
---
+++ Klasa 4/Zadanie_1/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window,  INotifyPropertyChanged
+    {
+        public string NameValue { get; set; }
+        public string SurnameValue { get; set; }
+        public string AgeValue { get; set; }
+
+
+        private string response;
+        public string Response
+        {
+            get { return response; }
+            set
+            {
+                response = value;
+                OnPropertyChanged(nameof(Response));
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            float wynik;
+            if(float.TryParse(AgeValue, out wynik) && NameValue.Trim() != "" && SurnameValue.Trim() !="")
+            {
+                if(wynik >= 18)
+                {
+                    Response = "Witaj " +NameValue +" "+SurnameValue +" jesteś pełnoletni";
+                }
+                else { Response = "Witaj " + NameValue + " " + SurnameValue + " jesteś kiddem"; }
+            }
+            else
+            {
+                Response = "Podaj poprawne dane";
+                if (NameValue == null) Response = Response + ", nie znam twojego imienia";
+                if (SurnameValue == null) Response = Response + ", nie znam twojego nazwiska";
+
+                Response = Response + " :(";
+            }
+        }
+    }
+}

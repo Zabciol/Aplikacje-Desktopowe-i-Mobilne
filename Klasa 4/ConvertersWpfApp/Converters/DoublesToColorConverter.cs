f446b940fb48572812d879533f225faed701e189
Zabciol <104020296+Zabciol@users.noreply.github.com>
2023-12-14 11:19:32 +0100

    Wsm wszystko i nic

---
+++ Klasa 4/ConvertersWpfApp/Converters/DoublesToColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ConvertersWpfApp.Converters
+{
+    public class DoublesToColorConverter : IMultiValueConverter
+    {
+        public object IMultiValueConverter.Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            Color color;
+            if(values != null)
+            {
+
+            }
+            throw new NotImplementedException();
+        }
+
+        public object[] IMultiValueConverter.ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Klasa 4/Suwaki_Kolor_RGB/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Suwaki_Kolor_RGB
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public double RedComponent {  get; set; }
+        public double GreenComponent {  get; set; }
+        public double BlueComponent {  get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
